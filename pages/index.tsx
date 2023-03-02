6e379c935efaa968f18fdfeb92ab1baacf745b0e
Keitu <modipakeitumetse@gmail.com>
2023-03-02 22:07:24 +0200

    Weather App Placeholder

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import DSA_App from "public/Data-structures-App-Image.png";
 import Eskom_App from "public/Eskom-App-Image.png";
+import Weather_App from "public/Weather_App.png";
 
 export default function Home() {
   return (
@@ -18,8 +19,18 @@ export default function Home() {
       <section className="projects">
         <h2 >PROJECTS</h2>
         <div className="projects-container">
-          <a href="https://dsa-app-tau.vercel.app/" target="_blank" rel="noreferrer noopener"> <Image src={DSA_App} alt="Thumnail of DSA-App" width="250" height="250" /></a>
-          <a href="https://github.com/Keitumtse14/proxy" target="_blank" rel="noreferrer noopener"> <Image src={Eskom_App} alt="Thumnail of Rolling blackouts app" width="250" height="250" /></a>
+          <div>
+            <div className="Data-structures-tooltip">Data structures app</div>
+            <a href="https://dsa-app-tau.vercel.app/" target="_blank" rel="noreferrer noopener"> <Image src={DSA_App} alt="Thumbnail of DSA-App" width="250" height="250" /></a>
+          </div>
+          <div>
+            <div className="Rolling-blackouts-tooltip ">Rolling blackouts app</div>
+            <a href="https://github.com/Keitumtse14/proxy" target="_blank" rel="noreferrer noopener"> <Image src={Eskom_App} alt="Thumbnail of Rolling blackouts app" width="250" height="250" /></a>
+          </div>
+          <div>
+            <div className=" ">Weather app (in progress) </div>
+            <a href="https://dsa-app-tau.vercel.app/" target="_blank" rel="noreferrer noopener"> <Image src={Weather_App} alt="Thumbnail of Weather App" width="250" height="250" /></a>
+          </div>
         </div>
       </section>
     </div>
