6f4cb019fe1807784380fd4aac7c8c6169aa66f2
Keitumetse <110659962+Keitumtse14@users.noreply.github.com>
2025-09-01 11:16:53 +0000

    fixed landing page design

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,29 @@ import Weather_App from "public/Weather_App.png";
 export default function Home() {
   return (
     <div className="container">
+
       <Head>
         <title>Keitumetse Modipa</title>
         <meta name="description" content="My projects" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="">
-        <h1>Keitumetse Modipa</h1>
+      <section className="hero">
+        <div className="email-section">
+          <a className="email-link" href="mailto:modipakeitumetse14@gmail.com?subject=Hello&body=I%20want%20to%20contact%20you">
+            modipakeitumetse14@gmail.com
+          </a>
+
+          <div className="line"></div>
+        </div>
+        <div className="name-role"><label className="role" >Front-End Web Developer</label>
+          <h1>Keitumetse Modipa</h1></div>
+        <button className="view-projects"> View My Projects</button>
+        <div className="socials">
+          <Image width={40} height={40} src="/linkedin-svgrepo-com 1.svg" alt="LinkedIn" className="svg-size" />
+          <Image width={40} height={40} src="/github-outline-fill-svgrepo-com 1.svg" alt="" className="svg-size" />
+          <Image width={40} height={40} src="/facebook.svg" alt="" className="svg-size" />
+          <div className="socials-line"></div>
+        </div>
       </section>
 
       <section className="projects">
